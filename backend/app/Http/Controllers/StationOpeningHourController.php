9ddc0b8b1a1f9ac0ae062233bce542dba533396c
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-15 09:41:30 +0100

    create StationOpeningHourController.php

---
+++ backend/app/Http/Controllers/StationOpeningHourController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StationOpeningHour;
+use Illuminate\Http\Request;
+
+class StationOpeningHourController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StationOpeningHour $stationOpeningHour)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StationOpeningHour $stationOpeningHour)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StationOpeningHour $stationOpeningHour)
+    {
+        //
+    }
+}
