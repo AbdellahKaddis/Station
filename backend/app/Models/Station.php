be8ea54e87e1169e85a04cf367da450f6c4f4247
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-15 09:30:49 +0100

    enhance Station model with new relationships/attributes

---
+++ backend/app/Models/Station.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Station extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'service_start_date',
+        'latitude',
+        'longitude',
+        'phone',
+        'email',
+        'status',
+    ];
+
+    public function openingHours()
+    {
+        return $this->hasMany(StationOpeningHour::class);
+    }
+}
