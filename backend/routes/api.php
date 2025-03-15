fc0f6c27b4f120afaf4aabd954af17bf09643552
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-15 09:42:16 +0100

    implement station routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\EmailCheckController;
 
+Route::apiResource('stations',StationController::class);
+Route::post('check-email', [EmailCheckController::class, 'checkEmail']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+require __DIR__.'/auth.php';
