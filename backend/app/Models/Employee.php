a5be7f8bcb42d3ef94810e7c3675ed1d9389ea15
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-15 10:43:52 +0100

    define many to many relationship with Stationss

---
+++ backend/app/Models/Employee.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+        'employee_cin',
+        'last_name',
+        'first_name',
+        'email',
+        'phone',
+        'gender',
+        'date_of_birth',
+        'address',
+        'nationality',
+        'status',
+        'cnss_number',
+        'contract_type',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
+
+    public function stations()
+    {
+        return $this->belongsToMany(Station::class, 'employee_station')
+                    ->withPivot('start_date', 'end_date');
+    }
+}
