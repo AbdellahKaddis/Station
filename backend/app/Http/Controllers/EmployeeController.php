be52abfc8f2f28005b8969ebad9d1ae20f09edaa
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-16 09:37:19 +0100

    implement employee crud

---
+++ backend/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEmployeeRequest;
+use App\Http\Requests\UpdateEmployeeRequest;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use App\Http\Resources\EmployeeResource;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return EmployeeResource::collection(Employee::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmployeeRequest $request)
+    {
+        $employee = Employee::create($request->validated());
+        return response()->json($employee);
+        return new EmployeeResource($employee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee $employee)
+    {
+        return new EmployeeResource($employee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmployeeRequest $request, Employee $employee)
+    {
+        $employee->update($request->validated());
+        return new EmployeeResource($employee);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        $employee->delete();
+        return response()->json(null, 204);
+    }
+}
