26e9106f02b57b242633d33ba4e9b236a71ad049
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-16 09:36:39 +0100

    implement employees employee-stations ressource

---
+++ backend/app/Http/Resources/EmployeeResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+        'employee_cin' => $this->employee_cin,
+        'first_name' => $this->first_name,
+        'last_name' => $this->last_name,
+        'email' => $this->email,
+        'phone' => $this->phone,
+        'gender' => $this->gender,
+        'date_of_birth' => $this->date_of_birth->format('Y-m-d'),
+        'address' => $this->address,
+        'nationality' => $this->nationality,
+        'status' => $this->status,
+        'cnss_number' => $this->cnss_number,
+        'contract_type' => $this->contract_type,
+        'created_at' => $this->created_at,
+        'updated_at' => $this->updated_at,
+        ];
+    }
+}
---
+++ backend/app/Http/Resources/EmployeeStationResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeStationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            
+            // 'id' => $this->id,
+            'employee' => [
+                'id' => $this->employee->id,
+                'name' => $this->employee->first_name . ' ' . $this->employee->last_name,
+            ],
+            'station' => [
+                'id' => $this->station->id,
+                'name' => $this->station->name,
+            ],
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+        ];
+    }
+}
