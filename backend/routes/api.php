9da34793738dccda6fb097631a44ae9523bd37ee
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-16 09:33:10 +0100

    implement employees and employee-stations routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StationController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EmailCheckController;
+use App\Http\Controllers\EmployeeStationController;
 
 Route::apiResource('stations',StationController::class);
+Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('employee-stations',EmployeeStationController::class);
 Route::post('check-email', [EmailCheckController::class, 'checkEmail']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
