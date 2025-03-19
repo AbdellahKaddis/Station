1278c7cbf043e0e381c3170bd12c417a56ce4b31
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-20 00:01:00 +0100

    add new routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,15 +2,31 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuelController;
+use App\Http\Controllers\PumpController;
+use App\Http\Controllers\TankController;
 use App\Http\Controllers\StationController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\FuelTypeController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\EmailCheckController;
+use App\Http\Controllers\StockEntryController;
+use App\Http\Controllers\NationalityController;
 use App\Http\Controllers\EmployeeStationController;
 
 Route::apiResource('stations',StationController::class);
 Route::apiResource('employees', EmployeeController::class);
 Route::apiResource('employee-stations',EmployeeStationController::class);
-Route::post('check-email', [EmailCheckController::class, 'checkEmail']);
+Route::apiResource('fuel-types',FuelTypeController::class);
+Route::apiResource('tanks',TankController::class);
+Route::apiResource('pumps', PumpController::class);
+Route::post('/check-email/{model}', [EmailCheckController::class, 'checkEmail']);
+Route::get('/nationalities',[NationalityController::class,'index']);
+
+
+Route::apiResource('suppliers', SupplierController::class);
+
+Route::apiResource('stock-entries', StockEntryController::class);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
