07aef650544840b4bb00b3eef620cd12122d054f
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-19 23:44:07 +0100

    Add TankController with CRUD operations

---
+++ backend/app/Http/Controllers/TankController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tank;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\TankResource;
+use App\Http\Requests\StoreTankRequest;
+use App\Http\Requests\UpdateTankRequest;
+
+class TankController extends Controller
+{
+    public function index()
+    {
+        $tanks = Tank::paginate(10); // Paginate results
+        return TankResource::collection($tanks);
+    }
+
+    public function store(StoreTankRequest $request)
+    {
+        $tank = Tank::create($request->validated());
+        return new TankResource($tank, Response::HTTP_CREATED);
+    }
+
+    public function show(Tank $tank)
+    {
+        return new TankResource($tank);
+    }
+
+    public function update(UpdateTankRequest $request, Tank $tank)
+    {
+        $tank->update($request->validated());
+        return new TankResource($tank);
+    }
+
+    public function destroy(Tank $tank)
+    {
+        $tank->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
