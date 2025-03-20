205fd6d1cc91f74e8bf6a0146a39a2fc93ca0db7
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-20 20:23:53 +0100

    Add migration for MeterReadings table

---
+++ backend/database/migrations/2025_03_20_080456_create_meter_readings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meter_readings', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('initial_reading', 10, 2); 
+            $table->decimal('final_reading', 10, 2)->nullable();
+            $table->unsignedBigInteger('pump_id');
+            $table->foreign('pump_id')->references('id')->on('pumps')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meter_readings');
+    }
+};
