3a7868d387e35524d99e4acef116b8a82998315b
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-21 08:16:06 +0100

    create  UpdatePlanningRequest and StorePlanningRequest

---
+++ backend/app/Http/Requests/UpdatePlanningRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePlanningRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'week' => 'sometimes|integer|min:1|max:52',
+            'day' => 'sometimes|integer|min:0|max:6',
+            'start_time' => 'sometimes|date_format:H:i',
+            'end_time' => 'sometimes|date_format:H:i|after:start_time',
+            'year' => 'sometimes|integer|min:2000|max:2100',
+            'employee_id' => 'sometimes|exists:employees,id',
+        ];
+    }
+}
