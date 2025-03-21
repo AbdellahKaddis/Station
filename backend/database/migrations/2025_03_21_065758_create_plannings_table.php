bdf8037be1cfc014f4f0db49ab18a6af9b24f377
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-21 08:13:04 +0100

    add migration for plannings table

---
+++ backend/database/migrations/2025_03_21_065758_create_plannings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plannings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('week'); 
+            $table->unsignedTinyInteger('day'); 
+            $table->time('start_time'); 
+            $table->time('end_time'); 
+            $table->unsignedSmallInteger('year'); 
+            $table->unsignedBigInteger('employee_id'); 
+            $table->timestamps(); 
+            // Foreign key constraint
+            $table->foreign('employee_id')
+                  ->references('id')
+                  ->on('employees')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plannings');
+    }
+};
