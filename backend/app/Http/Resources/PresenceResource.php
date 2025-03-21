511b06577be7a976e769b3b7a50e959032873d71
AbdellahKaddis <abdellahkaddis8@gmail.com>
2025-03-21 10:20:11 +0100

    add PresenceResource for Api responses

---
+++ backend/app/Http/Resources/PresenceResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PresenceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'week' => $this->week,
+            'day' => $this->day,
+            'start_time' => (new DateTime($this->start_time))->format('H:i'),  // Format start_time to 'HH:MM'
+            'end_time' => (new DateTime($this->end_time))->format('H:i'),  
+            'year' => $this->year,
+            'employee_id' => $this->employee_id,
+            'status' => $this->status,
+            'employee' => new EmployeeResource($this->whenLoaded('employee')), // Include employee data if loaded
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
